1664c8abb2a4fb9ba8056b51f5fd3e39ce674c40
Matthew Vallance <matthew.vallance@outlook.com>
2023-11-29 15:31:24 +0000

    KMeans working with CSV output

--- matthew/simple_kmeans_visualization/import_data.py
+++ matthew/simple_kmeans_visualization/import_data.py
@@ -5,25 +5,30 @@
 """
 import csv
 import random
-import testing_functions
+import pandas as pd
 
 
-def get_data(filename):
-    x = []
-    y = []
-    with open('example_data/'+filename, 'r') as file:
+def get_dataframe(filename):
+    with open('example_data/' + filename, 'r') as file:
         # Read CSV file
         data = csv.reader(file)
 
-        # Use a count to ignore headers
+        # Create a dataframe
         count = 0
         for line in data:
-            if count > 1:
-                x.append(int(line[0]))
-                y.append(int(line[1]))
+            temp_line_data = []
+            if count < 1:
+                df = pd.DataFrame(columns=line)
+                row_count = len(line)
+            else:
+                for col in range(0, row_count):
+                    temp_line_data.append(int(line[col]))
+                df.loc[count] = temp_line_data
+
             count += 1
 
-    return x, y
+    return df
+
 
 def create_random_chart_data():
     x = []
@@ -48,7 +53,10 @@ def create_random_chart_data():
 
     return x, y
 
+
 if __name__ == "__main__":
+    print(get_dataframe('segmentation_data.csv').head())
+    exit()
     x, y = create_random_chart_data()
     data = list(zip(x, y))
     testing_functions.write_to_csv(data, 'lrg_rand_clusters')
